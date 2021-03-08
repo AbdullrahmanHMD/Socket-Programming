7540fe3178838c9fa830cdc1f13ea901c3ac3889
Abdul <manofsteel272@yahoo.com>
2021-03-08 23:56:15 +0300

    Add AccessToken class

---
+++ Project - 01/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        User user1 = new User("Abdul Rahman", "12345bg");
+        AccessToken token1 = new AccessToken(user1, 14);
+
+        System.out.println(user1);
+        System.out.println(token1);
+    }
+
+}
